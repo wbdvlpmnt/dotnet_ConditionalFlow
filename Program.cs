68d948d7669b04c4a385768d697bb36c8f9636d0
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-06 12:15:17 -0600

    conditional flow

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine(ConditionalFlow.FindMax(1, 2));
+        System.Console.WriteLine(2 > 1 ? 2 : 1);
     }
+}
+
+public class ConditionalFlow()
+{
+
+    public static int FindMax(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+
 }
